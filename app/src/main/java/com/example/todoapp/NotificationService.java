1956a60ad019eb598a4cd8613e16df7e41a259f7
Cfnatic7 <78828448+Cfnatic7@users.noreply.github.com>
2023-04-23 15:15:58 +0200

    fix: start foreground

--- app/src/main/java/com/example/todoapp/NotificationService.java
+++ app/src/main/java/com/example/todoapp/NotificationService.java
@@ -30,16 +30,17 @@ public class NotificationService extends Service {
             Notification notification = createNotification(task);
             NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
 
-            if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.POST_NOTIFICATIONS) == PackageManager.PERMISSION_GRANTED) {
-                // TODO: Consider calling
-                //    ActivityCompat#requestPermissions
-                // here to request the missing permissions, and then overriding
-                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                //                                          int[] grantResults)
-                // to handle the case where the user grants the permission. See the documentation
-                // for ActivityCompat#requestPermissions for more details.
-                notificationManager.notify(NOTIFICATION_ID, notification);
-            }
+//            if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.POST_NOTIFICATIONS) == PackageManager.PERMISSION_GRANTED) {
+//                // TODO: Consider calling
+//                //    ActivityCompat#requestPermissions
+//                // here to request the missing permissions, and then overriding
+//                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+//                //                                          int[] grantResults)
+//                // to handle the case where the user grants the permission. See the documentation
+//                // for ActivityCompat#requestPermissions for more details.
+//                notificationManager.notify(NOTIFICATION_ID, notification);
+//            }
+        this.startForeground(NOTIFICATION_ID, notification);
 
         }
         return START_NOT_STICKY;
